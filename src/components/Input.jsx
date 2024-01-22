ab85a1bda04398b951e11d5506cd4178aacf780f
GabyAM <gdgabyam@gmail.com>
2024-01-22 11:11:24 -0300

    adds value prop to input component
    forgot it

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,13 @@
-export function Input({ changeCallback, label, name, type }) {
+export function Input({ changeCallback, label, name, type, value }) {
 	return (
 		<label>
 			{label}{" "}
-			<input type={type} name={name} onChange={changeCallback}></input>
+			<input
+				type={type}
+				name={name}
+				onChange={changeCallback}
+				defaultValue={value}
+			></input>
 		</label>
 	);
 }
